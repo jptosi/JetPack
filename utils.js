bc251d776946e8ec78a20ff47faec85746271dc9
jptosi <jp.tosi@gmail.com>
2021-02-01 14:52:42 +0100

    remove tools

--- utils.js
+++ utils.js
@@ -113,19 +113,3 @@ function getRandom(min, max, operator = 0) {
 function getRandomArray(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
-
-/** lerp
-    start - target - step
-**/
-function lerp(start, target, step){
-    return (1-step)*start + step*target;
-}
-
-/** mapScale
-    n : value on scale1
-    start1 : origine on scale 1 - end1 : end of scale1
-    start2 : origine on scale 2 - end2 : end of scale2
-**/
-function mapScale(n, start1, end1, start2, end2){
-    return (n - start1) / (end1 - start1) * (end2 - start2) + start2;
-}
